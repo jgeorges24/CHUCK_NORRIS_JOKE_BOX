3442830233bc570f397b94f03fda854c5424a7be
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-25 10:58:59 -0500

    testing the bin/run command and making sure the environment was set up properly

--- lib/api.rb
+++ lib/api.rb
@@ -12,9 +12,9 @@ class API
         puts "---------grabbed data---------"
         chuck_jokes_hash = HTTParty.get(@url)
         array_jokes = chuck_jokes_hash["value"]
-        array_jokes.each do |joke_hash| #may want to use .map to return all of them not just one at a time
+        array_jokes.map do |joke_hash| #may want to use .map to return all of them not just one at a time
         Chuck_Joke.new(joke_hash)
-        binding.pry
+        #binding.pry
          end
         #binding.pry
     end
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,22 +4,23 @@ require_relative '../config/environment.rb'
 class CLI
 
   def initialize
-    # API.new.get_chuck_jokes
-
+     API.new.get_chuck_jokes
   end
-
-      #  puts "this is cli file"
+  
+      #  ____------THIS IS WHERE WE START _____-----------
       def run
         puts "hello"
         sleep(1)
         puts "whats is thy name?"
-        greeting(user_input)
-        #menu
+        greeting(user_input) #<<--- check greeting method to see whats going on
+        menu #<----- at this pointe we are asking user for y or n controle flow
         #user_input
-        API.new.get_chuck_jokes
+        #API.new.get_chuck_jokes TOSSED THIS IN INITIALIZE TO GET THIS UPON INSTANCE.
         #display
         # Chuck_Jokes.new.(jokes_data)
       end
+
+ #<<----------DEFINED METHODS BELOW ----------->>>>
 
       def greeting(name)
         puts "Welcome #{name}, this is my sancuary of The Great Chuck Norris!"
@@ -28,20 +29,32 @@ class CLI
         sleep(1)
         puts "So do you want to hear some jokes or nah? yes or no?"
       end
-      
+
+
       def menu
         #code for the menu goes here...
         selection = user_input
 
         if selection == "yes"
           #something happens
+
         elsif selection == "no"
           #this happens...
+          goodbye
         else
           #okay this was an invalid response...
+          invalid_command
         end
 
-        
+      end
+
+      def goodbye
+        puts "Thanks for coming to my mom's house...stop bye for some more fun..."
+      end
+
+      def invalid_command #<<<---------===== side not used menu method in  this method!!!
+        puts "HOW DARE YOU!? this is not what i asked of you... yes or no?!?"
+        menu
       end
 
       def display
